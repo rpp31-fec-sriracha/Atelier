3ff1dcef958b77e0d55f4e2fe5fc8669c9f80c53
Jake Caughern <jake.caughern@gmail.com>
2021-10-26 22:51:06 -0500

    created sample data

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,64 +1,66 @@
 import React from 'react';
 import ProductInfo from './ProductInfo.jsx';
+import sampleData from './sampleData.js';
 
-const getData = function(endpoint, params, callback) {
-  const TOKEN = '';
-  const axios = require('axios').default;
+// const getData = function(endpoint, params, callback) {
+//   const TOKEN = '';
+//   const axios = require('axios').default;
 
-  const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
+//   const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
 
-  axios.get(`${url}${endpoint}`, {
-    headers: { 'Authorization': TOKEN },
-    params: params,
-  })
-    .then((response) => callback(null, response.data))
-    .catch((error) => callback(error, null));
+//   axios.get(`${url}${endpoint}`, {
+//     headers: { 'Authorization': TOKEN },
+//     params: params,
+//   })
+//     .then((response) => callback(null, response.data))
+//     .catch((error) => callback(error, null));
 
-};
+// };
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: {},
-      styles: {},
+      product: sampleData.product,
+      styles: sampleData.styles,
       loadProductInfo: false,
     };
   }
 
   componentDidMount() {
-    getData(
-      '/products', {
-        page: 1,
-        count: 5,
-        responseType: 'json'
-      },
-      (err, data) => {
-        if (err) {
-          console.log(err);
-        } else {
-          this.setState({ product: data[1] });
-          getData(
-            `/products/${data[1].id}/styles`,
-            { responseType: 'json' },
-            (err, styles) => {
-              if (err) {
-                console.log(err);
-              } else {
-                this.setState({
-                  styles,
-                  loadProductInfo: true,
-                });
-                this.render();
-              }
-            }
-          );
-        }
-      });
+    // getData(
+    //   '/products', {
+    //     page: 1,
+    //     count: 5,
+    //     responseType: 'json'
+    //   },
+    //   (err, data) => {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //       this.setState({ product: data[1] });
+    //       getData(
+    //         `/products/${data[1].id}/styles`,
+    //         { responseType: 'json' },
+    //         (err, styles) => {
+    //           if (err) {
+    //             console.log(err);
+    //           } else {
+    //             this.setState({
+    //               styles,
+    //               loadProductInfo: true,
+    //             });
+    //             this.render();
+    //           }
+    //         }
+    //       );
+    //     }
+    //   });
 
   }
 
   render() {
+    console.log(sampleData);
     return (<div className="overview">
       <ProductInfo product={this.state.product} styles={this.state.styles} />
     </div>);
---
+++ client/src/components/Overview/sampleData.js
@@ -0,0 +1,112 @@
+const dataString = `{
+  "product": {
+    "id": 59554,
+    "campus": "hr-rpp",
+    "name": "Bright Future Sunglasses",
+    "slogan": "You've got to wear shades",
+    "description": "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
+    "category": "Accessories",
+    "default_price": "69.00",
+    "created_at": "2021-10-18T22:50:41.839Z",
+    "updated_at": "2021-10-18T22:50:41.839Z",
+    "features": [
+        {
+            "feature": "Lenses",
+            "value": "Ultrasheen"
+        },
+        {
+            "feature": "UV Protection",
+            "value": null
+        },
+        {
+            "feature": "Frames",
+            "value": "LightCompose"
+        }
+    ]
+  },
+  "styles": {
+    "product_id": "59554",
+    "results": [
+        {
+            "style_id": 365419,
+            "name": "Black Lenses & Black Frame",
+            "original_price": "69.00",
+            "sale_price": null,
+            "default?": false,
+            "photos": [
+                {
+                    "thumbnail_url": null,
+                    "url": null
+                }
+            ],
+            "skus": {
+                "null": {
+                    "quantity": null,
+                    "size": null
+                }
+            }
+        },
+        {
+            "style_id": 365420,
+            "name": "Black Lenses & Gold Frame",
+            "original_price": "69.00",
+            "sale_price": null,
+            "default?": true,
+            "photos": [
+                {
+                    "thumbnail_url": null,
+                    "url": null
+                }
+            ],
+            "skus": {
+                "null": {
+                    "quantity": null,
+                    "size": null
+                }
+            }
+        },
+        {
+            "style_id": 365421,
+            "name": "Gold Lenses & Black Frame",
+            "original_price": "69.00",
+            "sale_price": null,
+            "default?": false,
+            "photos": [
+                {
+                    "thumbnail_url": null,
+                    "url": null
+                }
+            ],
+            "skus": {
+                "null": {
+                    "quantity": null,
+                    "size": null
+                }
+            }
+        },
+        {
+            "style_id": 365422,
+            "name": "Gold Lenses & Gold Frame",
+            "original_price": "69.00",
+            "sale_price": null,
+            "default?": false,
+            "photos": [
+                {
+                    "thumbnail_url": null,
+                    "url": null
+                }
+            ],
+            "skus": {
+                "null": {
+                    "quantity": null,
+                    "size": null
+                }
+            }
+        }
+    ]
+  }
+}`;
+
+const sampleData = JSON.parse(dataString);
+
+export default sampleData;
