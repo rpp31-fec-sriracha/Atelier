3fa7bcf4538cf61de0d8ce344adbc5910ee141f1
Jake Caughern <jake.caughern@gmail.com>
2021-11-02 19:51:42 -0500

    starting writing intial tests

---
+++ test/overview.test.js
@@ -0,0 +1,13 @@
+// import dependencies
+import React from 'react';
+
+// import API mocking utilities from Mock Service Worker
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
+
+// import react-testing methods
+import {render, fireEvent, waitFor, screen} from '@testing-library/react';
+
+// add custom jest matchers from jest-dom
+import '@testing-library/jest-dom';
+// import ../client/src/
