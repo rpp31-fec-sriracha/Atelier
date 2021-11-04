b0c47b127ffe0c77201910bbdaf343f9f1f726cb
Maxine <maxine.sejin.seo@gmail.com>
2021-11-03 23:36:51 -0700

    Implemented working Answer Modal

--- client/src/components/Questions/AnswerModal.jsx
+++ client/src/components/Questions/AnswerModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
 class AnswerModal extends React.Component {
   constructor(props) {
@@ -14,28 +15,35 @@ class AnswerModal extends React.Component {
   // invoke handle add answer
   // handle input change
   render() {
-    const { question, productInfo } = this.props;
+    const { question, productInfo, isOpen, closeModal } = this.props;
+    console.log(closeModal);
     const { answer, nickname, email, photos } = this.state;
 
-    return (
+    return ReactDom.createPortal(
       <>
-        <div className="answer-modal">
-          <form onSubmit={this.handleAddAnswer}>
-            <div className="modal-title">Submit your Answer</div>
-            <div className="modal-subtitle">{productInfo}: {question.question_body}</div>
-            <label>Your Answer</label><span id="mandatory-asterisk">*</span>
-            <textarea value={answer} name="answer"></textarea>
-            <label>What is your nickname</label><span id="mandatory-asterisk">*</span>
-            <input type="text" value={nickname} name="nickname" placeholder="Example: jack543!"></input>
-            <label>Your email</label><span id="mandatory-asterisk">*</span>
-            <input type="email" value={email} name="email" placeholder="Example: jack@email.com"></input>
-            <label>Upload your photos</label>
-            <input type="file" name="photo"></input>
+        {isOpen ? (
+          <>
+            <div className="overlay-style"></div>
+            <div className="modal">
+              <form onSubmit={() => closeModal()}>
+                <div className="modal-title">Submit your Answer</div>
+                <div className="modal-subtitle">{productInfo}: {question.question_body}</div>
+                <label>Your Answer</label><span id="mandatory-asterisk">*</span>
+                <textarea value={answer} name="answer"></textarea>
+                <label>What is your nickname</label><span id="mandatory-asterisk">*</span>
+                <input type="text" value={nickname} name="nickname" placeholder="Example: jack543!"></input>
+                <label>Your email</label><span id="mandatory-asterisk">*</span>
+                <input type="email" value={email} name="email" placeholder="Example: jack@email.com"></input>
+                <label>Upload your photos</label>
+                <input type="file" name="photo"></input>
 
-            <input type="submit" value="Submit answer"></input>
-          </form>
-        </div>
-      </>
+                <input type="submit" value="Submit answer"></input>
+              </form>
+            </div>
+          </>) : null}
+
+      </>,
+      document.getElementById('answer-portal')
     );
   }
 }
--- client/src/components/Questions/QuestionEntry.jsx
+++ client/src/components/Questions/QuestionEntry.jsx
@@ -2,39 +2,52 @@ import React, { useState } from 'react';
 import AnswerEntry from './AnswerEntry.jsx';
 import AnswerModal from './AnswerModal.jsx';
 
-const QuestionEntry = ({ question, productInfo }) => {
-  const [isOpen, setOpen] = useState(false);
-  // handle open modal
-  // handle close modal
+class QuestionEntry extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false
+    };
+  }
+  openModal() {
+    this.setState({ isOpen: true });
+  }
+  closeModal() {
+    this.setState({ isOpen: true });
+  }
   // handle report question
   // handle mark helpful
   // handle add answer
   // render < AnswerModal >
-  return (
-    <>
-      <div className="question">
-        <span className="Q">Q:</span>
-        <span className="Q-body">{question.question_body}</span>
-        <div>
-          <div className="helpful">
-            <span>Helpful?</span>
-            <button className="increment-helpful">Yes({question.question_helpfulness})</button>
-            <span class="_divider">|</span>
-            <a href="#answer-modal" className="add-answer">Add Answer</a>
-          </div>
-          <AnswerModal id="#answer-modal" question={question} productInfo={productInfo} isOpen={isOpen} />
-        </div>
-        <div className="answers-list">
-          <span className="A">A:</span>
-          {Object.keys(question.answers).map((id, i) => {
-            let answer = question.answers[id];
-            return <AnswerEntry key={i} answer={answer} />;
-          })}
-        </div>
-      </div>
+  render() {
+    const { question, productInfo } = this.props;
+    const { isOpen } = this.state;
 
-    </>
-  );
-};
+    return (
+      <>
+        <div id="question">
+          <span className="Q">Q:</span>
+          <span className="Q-body">{question.question_body}</span>
+          <div>
+            <div className="helpful">
+              <span>Helpful?</span>
+              <button className="increment-helpful">Yes({question.question_helpfulness})</button>
+              <span class="_divider">|</span>
+              <a href="#answer-modal" className="add-answer" onClick={() => this.openModal()}>Add Answer</a>
+            </div>
+            <AnswerModal isOpen={isOpen} id="#answer-modal" closeModal={this.closeModal.bind(this)} question={question} productInfo={productInfo}></AnswerModal>
+          </div>
+          <div className="answers-list">
+            <span className="A">A:</span>
+            {Object.keys(question.answers).map((id, i) => {
+              let answer = question.answers[id];
+              return <AnswerEntry key={i} answer={answer} />;
+            })}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default QuestionEntry;
