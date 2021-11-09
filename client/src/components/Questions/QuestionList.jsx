804c81254067427a4903b240edb483ab185f9b2a
Maxine <maxine.sejin.seo@gmail.com>
2021-11-08 21:01:50 -0800

    Moved Add question Modal to Question list component

--- client/src/components/Questions/QuestionList.jsx
+++ client/src/components/Questions/QuestionList.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 import QuestionEntry from './QuestionEntry.jsx';
+import QuestionModal from './QuestionModal.jsx';
 
-const QuestionList = function ({ questions, productInfo }) {
+const QuestionList = function ({ questions, productInfo, handleAddQuestion }) {
   const [visibleCount, setVisibleCount] = useState(2);
+  const [isOpen, setModal] = useState(false);
+
+  const openModal = () => {
+    setModal(true);
+  };
+  const closeModal = () => {
+    setModal(false);
+  };
+
 
   return (
     <>
@@ -14,6 +24,14 @@ const QuestionList = function ({ questions, productInfo }) {
         {(questions.length > 0) ?
           <button className="col-1-3" onClick={() => setVisibleCount(visibleCount + 2)}>MORE ANSWERED QUESTIONS</button>
           : <div></div>}
+        <button className="add-q" role="add-quesiton" onClick={() => openModal()}>AddQuestion</button>
+        <QuestionModal
+          role="q-modal"
+          isOpen={isOpen}
+          productInfo={productInfo}
+          closeModal={closeModal.bind(this)}
+          handleAddQuestion={handleAddQuestion}>
+        </QuestionModal>
       </div>
     </>
   );
