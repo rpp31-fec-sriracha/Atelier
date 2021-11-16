7322395e3c55e6d5fca779f5b4e1c93ceb1260b5
Rick Kunz <rick.c.kunz@gmail.com>
2021-11-16 14:37:24 -0800

    Added additional tests

--- test/reviews.test.js
+++ test/reviews.test.js
@@ -9,6 +9,7 @@ import '@testing-library/jest-dom';
 import { unmountComponentAtNode } from 'react-dom';
 import Reviews from '../client/src/components/Reviews/Reviews.jsx';
 import IndividualReviewTile from '../client/src/components/Reviews/IndividualReviewTile.jsx';
+import NewReview from '../client/src/components/Reviews/NewReview.jsx';
 import sampleReviewData from './reviewsData.js';
 
 const server = setupServer(
@@ -33,17 +34,29 @@ it('should render with sample data', () => {
 
 describe('Individual Reviews', () => {
   describe('Single Review', () => {
-    test('renders how helpful the review is', () => {
+    test('renders the name of the reviewer', () => {
       let sampleReview = sampleReviewData[0].results[0];
 
       render(<IndividualReviewTile currentReview={sampleReview}></IndividualReviewTile>);
-      screen.debug();
-      // screen.getByText
+      // screen.debug();
+      screen.getByText(/shopaddict/);
       // screen.getByRole
+    });
+  });
+});
 
-      // waitFor(() => findByText('Report'))
-      //   .then(() => expect(screen.findByTestId('helpful-count')).toHaveTextContent('2'))
-      //   .catch((err) => console.log(err));
+
+describe('New Reviews', () => {
+  describe('New Review text', () => {
+    test('renders the product name at the top of a new review form', () => {
+
+      let sampleCharacteristics = {"Fit":{"id":199854,"value":"3.4444444444444444"},"Length":{"id":199855,"value":"3.0000000000000000"},"Comfort":{"id":199856,"value":"3.4444444444444444"},"Quality":{"id":199857,"value":"3.7222222222222222"}};
+
+      render(<NewReview productName={'Slacker\'s Slacks'} open={true}
+        characteristics={sampleCharacteristics} productID={59556}></NewReview>);
+      screen.debug();
+      screen.getByText('About the Slacker\'s Slacks.');
+      // screen.getByRole
     });
   });
 });
