5dce6d8ba6ba5b7cd5c466fbc3c99b6c51d8e7e9
Rick Kunz <rick.c.kunz@gmail.com>
2021-11-19 15:10:32 -0800

    Adding tests

--- test/reviews.test.js
+++ test/reviews.test.js
@@ -45,18 +45,29 @@ describe('Individual Reviews', () => {
   });
 });
 
+describe('Product Breakdown', () => {
+  test('renders the name of the reviewer', () => {
+    let sampleReview = sampleReviewData[0].results[0];
 
-describe('New Reviews', () => {
-  describe('New Review text', () => {
-    test('renders the product name at the top of a new review form', () => {
-
-      let sampleCharacteristics = {"Fit":{"id":199854,"value":"3.4444444444444444"},"Length":{"id":199855,"value":"3.0000000000000000"},"Comfort":{"id":199856,"value":"3.4444444444444444"},"Quality":{"id":199857,"value":"3.7222222222222222"}};
-
-      render(<NewReview productName={'Slacker\'s Slacks'} open={true}
-        characteristics={sampleCharacteristics} productID={59556}></NewReview>);
-      screen.debug();
-      screen.getByText('About the Slacker\'s Slacks.');
-      // screen.getByRole
-    });
+    render(<IndividualReviewTile currentReview={sampleReview}></IndividualReviewTile>);
+    // screen.debug();
+    screen.getByText(/shopaddict/);
+    // screen.getByRole
   });
 });
+
+
+// describe('New Reviews', () => {
+//   describe('New Review text', () => {
+//     test('renders the product name at the top of a new review form', () => {
+
+//       let sampleCharacteristics = {"Fit":{"id":199854,"value":"3.4444444444444444"},"Length":{"id":199855,"value":"3.0000000000000000"},"Comfort":{"id":199856,"value":"3.4444444444444444"},"Quality":{"id":199857,"value":"3.7222222222222222"}};
+
+//       render(<NewReview productName={'Slacker\'s Slacks'} open={true}
+//         characteristics={sampleCharacteristics} productID={59556}></NewReview>);
+//       screen.debug();
+//       screen.getByText('About the Slacker\'s Slacks.');
+//       // screen.getByRole
+//     });
+//   });
+// });
